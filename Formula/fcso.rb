d1099e89eb5e8312b7d83d1ed1863c3361963626
Bannon Tanner <bannon.n.tanner@gmail.com>
2024-09-22 14:16:02 -0500

    Update fcso.rb

--- Formula/fcso.rb
+++ Formula/fcso.rb
@@ -1,10 +1,10 @@
 class Fcso < Formula
     desc "A script that filters files/folders using command-line arguments and a config file"
     homepage "https://github.com/tannerabread/folder-content-spitter-outter"
-    url "https://github.com/tannerabread/folder-content-spitter-outter/archive/v1.1.0.tar.gz"
-    # curl -L https://github.com/tannerabread/folder-content-spitter-outter/archive/v1.1.0.tar.gz | shasum -a 256
-    sha256 "bc0fd754d5f5b04d4b43e86ea15c0784096078d4e73e8eb62747a799b777ddc7"
-    version "1.1.0"
+    url "https://github.com/tannerabread/folder-content-spitter-outter/archive/v1.2.0.tar.gz"
+    # curl -L https://github.com/tannerabread/folder-content-spitter-outter/archive/v1.2.0.tar.gz | shasum -a 256
+    sha256 "fb768886a3678df48566e0a045e8dfa627b98dc9c757c5a7eb2c69ccf6f6ab9a"
+    version "1.2.0"
   
     def install
       bin.install "folder-content-spitter-outter.sh" => "fcso"
