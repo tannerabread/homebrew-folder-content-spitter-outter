deadeb6c1eb4a58aad61445d308ee46e908b9012
Bannon Tanner <bannon.n.tanner@gmail.com>
2024-09-21 22:49:49 -0500

    Create folder-content-spitter-outter.rb

---
+++ Formula/folder-content-spitter-outter.rb
@@ -0,0 +1,12 @@
+class FilterScript < Formula
+    desc "A script that filters files/folders using command-line arguments and a config file"
+    homepage "https://github.com/tannerabread/folder-content-spitter-outter"
+    url "https://github.com/tannerabread/folder-content-spitter-outter/archive/v1.0.0.tar.gz"
+    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    version "1.0.0"
+  
+    def install
+      bin.install "folder-content-spitter-outter.sh" => "folder-content-spitter-outter"
+    end
+  end
+  
