6cb6f8a7a962c6fd44852e8b18b34dac994ed45e
Bannon Tanner <bannon.n.tanner@gmail.com>
2024-09-22 10:08:52 -0500

    Update fcso.rb

--- Formula/fcso.rb
+++ Formula/fcso.rb
@@ -2,7 +2,7 @@ class Fcso < Formula
     desc "A script that filters files/folders using command-line arguments and a config file"
     homepage "https://github.com/tannerabread/folder-content-spitter-outter"
     url "https://github.com/tannerabread/folder-content-spitter-outter/archive/v1.0.1.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "c6d032d66696961616a1174c6d1d6e81fe5dcc1792584da958f6c29bcbbd103d"
     version "1.0.1"
   
     def install
